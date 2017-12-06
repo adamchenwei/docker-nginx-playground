dd8f391facf8bd697c7856034b0a42d88989871b
Adam Chen Wei <awei@pebblepost.com>
2017-12-06 16:07:58 -0500

    chore(): all sudo for ubuntu

--- buildServerOnUbuntu.sh
+++ buildServerOnUbuntu.sh
@@ -16,25 +16,25 @@ sed -i "s/proxy_pass http:\/\/myip:9010\/api\/login;/proxy_pass http:\/\/"$myip"
 DOCKER_SERVICE_NAME='contentserver'
 
 printf ".... 1.1 list all running containers\n"
-docker container ls
+sudo docker container ls
 printf ".... 1.2  stop and removal of current content server...\n"
 sudo docker ps -q --filter ancestor="$DOCKER_SERVICE_NAME" | xargs -r sudo docker stop
 sudo docker ps -q --filter ancestor="$DOCKER_SERVICE_NAME" | xargs -r sudo docker rm
 sudo docker ps -q --filter ancestor="$DOCKER_SERVICE_NAME" --filter "status=exited" | xargs -r sudo docker rm
 printf ".... 2.1 list all images before delete\n"
-docker images
+sudo docker images
 printf ".... 2.2 remove docker image for $DOCKER_SERVICE_NAME...\n"
-docker rmi $DOCKER_SERVICE_NAME
+sudo docker rmi $DOCKER_SERVICE_NAME
 printf ".... 2.3 list all images after delete\n"
-docker images
+sudo docker images
 printf ".... 2.4 build content server image again...\n"
-docker build -t $DOCKER_SERVICE_NAME .
+sudo docker build -t $DOCKER_SERVICE_NAME .
 
 
 
 printf "32769:80 -> 80:80"
 printf ".... 3 start content server...\n"
-docker run -d -p 80:80 $DOCKER_SERVICE_NAME
+sudo docker run -d -p 80:80 $DOCKER_SERVICE_NAME
 
 printf ".... 4 show running server info...\n"
-docker container ls
+sudo docker container ls
